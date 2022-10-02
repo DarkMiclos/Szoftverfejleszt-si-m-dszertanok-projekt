6c41ede8b9e2d9c876a4cac6da12345622ef4ec7
smarko1 <sarbakmarko10@gmail.com>
2022-10-02 22:19:35 +0200

    Register/Login system

--- backend/database.js
+++ backend/database.js
@@ -3,23 +3,27 @@ const sqlite3 = require('sqlite3').verbose();
 function openDatabase() {
     return new sqlite3.Database("./database.db", sqlite3.OPEN_READWRITE, (err) => {
         if (err) return console.error(err.message);
-    
+
         console.log("connection succesfull");
     });
 }
 
-function closeDatabase(db) {//Call it if we dont want to do anything anymore in the database
+function closeDatabase() {//Call it if we dont want to do anything anymore in the database
     db.close((err) => {
         if (err) return console.error(err.message);
     })
 }
 
 
-function createUserTable(db) {//Only creates table if it not exists
+function createUserTable() {//Only creates table if it not exists
+    db = openDatabase();
     db.run("CREATE TABLE IF NOT EXISTS user(userId INTEGER NOT NULL PRIMARY KEY, userName TEXT, passwordHash TEXT)");
+    closeDatabase(db);
 }
 
-function createNewUser(db, userName, password) {
+function createNewUser(userName, password) {
+    db = openDatabase();
+    console.log(password);
     passwordHash = password
     //Todo hash password
     db.run("INSERT INTO user(userId, userName, passwordHash) VALUES(NULL,?,?)", [userName, passwordHash], (err) => {
@@ -28,19 +32,21 @@ function createNewUser(db, userName, password) {
         }
         console.log("Row was added to the table");
     })
-    
+    closeDatabase(db);
 }
 
-function getUserTable(db) {
+function getUserTable() {
+    db = openDatabase();
     let sql = "SELECT * FROM user"
     db.all(sql, [], (err, rows) => {
         if (err) {
           throw err;
         }
         rows.forEach((row) => {
-          console.log(row.userId);
+          console.log(row.userId, row.userName, row.passwordHash);
         });
       });
+    closeDatabase(db);
 }
 
 
